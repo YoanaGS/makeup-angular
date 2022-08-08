a2306d06e9aba29b83f625e5029061043fdaf815
Zhuan Mustafa <zhuan.mustafa@goambio.com>
2022-08-08 14:36:06 +0300

    nav-bar and home page wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TopBarComponent } from 'src/top-bar/top-bar.component';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from 'src/top-bar/home/home.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    RouterModule,
+    AppRoutingModule,
+    MatIconModule,
+    IvyCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
